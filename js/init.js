8e710c44021b9174f7b4f3d4f04ba0bbebe0908f
小傅Fox <xfox@cotr.me>
2015-02-09 23:12:38 +0800

    首个版本完成~
    注册0.1
    发布版本(-∞,0.5]

---
+++ js/init.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#header').load('/header.html', '', function() {
+    $.material.init();
+  });
+  $('#footer').load('/footer.html', '', function() {
+    $.material.init();
+  });
+});
